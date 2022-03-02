5959d7c828bf69d2d637a7eea94bbbf05971670a
rezamandaa <rezamandaa@gmail.com>
2022-03-02 16:39:09 +0700

    temperature converter fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -6,34 +5,102 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp>{
+  TextEditingController input = new TextEditingController();
+
+  double celcius = 0;
+  double kelvin = 0;
+  double reamur = 0;
+
+  void konversi() {
+    setState((){
+      celcius = double.parse(input.text);
+      kelvin = celcius + 273.15;
+      reamur = celcius * 0.8;
+    });
+  }
+   @override
   Widget build(BuildContext context) {
-    
-  
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Konverter Suhu',
       theme: ThemeData(
-        
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Konverter Suhu")),
+          title: const Text("Konverter Suhu")),
           body: Container(
-            margin: EdgeInsets.all(8),
-            // ignore: unnecessary_new
-            child:new Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                TextField(
-                decoration: InputDecoration(hintText: "Masukkan Suhu Dalam Celcius",),
+            margin: const EdgeInsets.all(8),
+            child:Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children:[
+                TextFormField(
+                  controller: input,
+                decoration: const InputDecoration(hintText: "Masukkan Suhu Dalam Celcius",),
                  keyboardType: TextInputType.number,
                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                )
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Column(
+                      children: [
+                        Text(
+                          "Suhu Dalam Kelvin",
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w300,
+                          ),
+                        ),
+                        Text(
+                          "$kelvin",
+                          style: TextStyle(
+                            fontSize: 40,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          "Suhu Dalam Reamur",
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w300,
+                          ),
+                        ),
+                        Text(
+                          "$reamur",
+                          style: TextStyle(
+                            fontSize: 40,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 50,
+                  child: TextButton(
+                    style: TextButton.styleFrom(backgroundColor: Colors.blue),
+                    onPressed: konversi,
+                    child: Text(
+                      "Konversi Suhu",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
               ],
             ) 
           ),
@@ -43,23 +110,48 @@ class MyApp extends StatelessWidget {
 }
 
 
-
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
-  
+
   @override
-  State<StatefulWidget> createState() {
-    return new HomePageState();
-  }
+  State<MyHomePage> createState() => _MyHomePageState();
 }
-  
-  class HomePageState extends State<MyHomePage> {
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      backgroundColor: Colors.white,
-     
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
